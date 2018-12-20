7a611d8bddcaa3b87cfd37efab6a911c0273ccd4
Joe Topjian <joe@topjian.net>
2018-12-20 05:08:33 +0000

    wip2

--- openstack/lb_v2_shared.go
+++ openstack/lb_v2_shared.go
@@ -51,7 +51,7 @@ func waitForLBV2Listener(lbClient *gophercloud.ServiceClient, listener *listener
 	} else {
 		if len(listener.Loadbalancers) > 0 {
 			lbID := listener.Loadbalancers[0].ID
-			refreshFunc = resourceLBV2LoadBalancerRefreshFunc(lbClient, lbID)
+			refreshFunc = resourceLBV2LoadBalancerStatusRefreshFunc(lbClient, lbID, "listener", listener.ID)
 		}
 	}
 
@@ -144,7 +144,7 @@ func waitForLBV2Member(lbClient *gophercloud.ServiceClient, parentPool *pools.Po
 			return err
 		}
 
-		refreshFunc = resourceLBV2LoadBalancerRefreshFunc(lbClient, lbID)
+		refreshFunc = resourceLBV2LoadBalancerStatusRefreshFunc(lbClient, lbID, "member", member.ID)
 	}
 
 	stateConf := &resource.StateChangeConf{
@@ -193,7 +193,7 @@ func waitForLBV2Monitor(lbClient *gophercloud.ServiceClient, parentPool *pools.P
 			return err
 		}
 
-		refreshFunc = resourceLBV2LoadBalancerRefreshFunc(lbClient, lbID)
+		refreshFunc = resourceLBV2LoadBalancerStatusRefreshFunc(lbClient, lbID, "monitor", monitor.ID)
 	}
 
 	stateConf := &resource.StateChangeConf{
@@ -241,7 +241,7 @@ func waitForLBV2Pool(lbClient *gophercloud.ServiceClient, pool *pools.Pool, targ
 			return err
 		}
 
-		refreshFunc = resourceLBV2LoadBalancerRefreshFunc(lbClient, lbID)
+		refreshFunc = resourceLBV2LoadBalancerStatusRefreshFunc(lbClient, lbID, "pool", pool.ID)
 	}
 
 	stateConf := &resource.StateChangeConf{
@@ -298,142 +298,60 @@ func lbV2FindLBIDviaPool(lbClient *gophercloud.ServiceClient, pool *pools.Pool) 
 	return "", fmt.Errorf("Unable to determine loadbalancer ID from pool %s", pool.ID)
 }
 
-// Function to detect the LB element provisioning status
-func lbV2GetProvisioningStatus(lbClient *gophercloud.ServiceClient,
-	statusFunc func(*gophercloud.ServiceClient, *string) (interface{}, string, error),
-	id string,
-	lbID *string,
-	target string) (interface{}, string, error) {
-
-	log.Printf("[DEBUG] Detecting LBaaSv2 status for the %s using the %s client", id, lbClient.Type)
-
-	res, status, err := statusFunc(lbClient, lbID)
-	if status != "" {
-		return res, status, err
-	}
-
-	log.Printf("[DEBUG] %s, falling back to resolve function", err)
-
-	if lbID != nil && *lbID != "" {
-		return lbV2GetProvisioningStatusViaLB(lbClient, id, *lbID)
-	}
-
-	log.Printf("[DEBUG] %s, falling back to heavy resolve function", err)
-
-	// Heavy API calls begin
-	lbsPages, err := loadbalancers.List(lbClient, loadbalancers.ListOpts{}).AllPages()
-	if err != nil {
-		return nil, "", fmt.Errorf("Failed to list Load Balancers: %s", err)
-	}
-
-	lbs, err := loadbalancers.ExtractLoadBalancers(lbsPages)
-	if err != nil {
-		return nil, "", fmt.Errorf("Failed to extract Load Balancers list into the object: %s", err)
-	}
-
-	for _, lb := range lbs {
-		// Query each Load Balancer we have
-		res, status, err := lbV2GetProvisioningStatusViaLB(lbClient, id, lb.ID)
-		if err == nil {
-			if lbID != nil {
-				*lbID = lb.ID
-				// Cache parent Load Balancer ID
-				log.Printf("[DEBUG] Cached %s Load Balancer ID", *lbID)
-			}
-			return res, status, nil
+func resourceLBV2LoadBalancerStatusRefreshFunc(lbClient *gophercloud.ServiceClient, lbID, resourceType, resourceID string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		statuses, err := loadbalancers.GetStatuses(lbClient, lbID).Extract()
+		if err != nil {
+			return nil, "", fmt.Errorf("Unable to get statuses from the Load Balancer %s statuses tree: %s", lbID, err)
 		}
-		log.Printf("[DEBUG] %s", err)
-	}
 
-	err404 := gophercloud.ErrDefault404{
-		gophercloud.ErrUnexpectedResponseCode{
-			BaseError: gophercloud.BaseError{
-				DefaultErrString: fmt.Sprintf("No %s resource found", id)},
-		},
-	}
-
-	return nil, "", err404
-}
-
-func lbV2GetProvisioningStatusViaLB(lbClient *gophercloud.ServiceClient, id string, lbID string) (interface{}, string, error) {
-	log.Printf("[DEBUG] Trying to detect %s object status from the Load Balancer %s statuses tree", id, lbID)
-	statuses, err := loadbalancers.GetStatuses(lbClient, lbID).Extract()
-	if err != nil {
-		return nil, "", fmt.Errorf("Unable to get statuses from the Load Balancer %s statuses tree: %s", lbID, err)
-	}
-
-	for _, listener := range statuses.Loadbalancer.Listeners {
-		if listener.ID == id {
-			if listener.ProvisioningStatus == "" {
-				log.Printf("[DEBUG] Got an empty provisioning status response for the %s Listener, falling back to ACTIVE", id)
-				return listener, "ACTIVE", nil
-			}
-			log.Printf("[DEBUG] Found %s provisioning status for the %s Listener", listener.ProvisioningStatus, id)
-			return listener, listener.ProvisioningStatus, nil
-		}
-		/* Waiting for https://github.com/gophercloud/gophercloud/issues/1366
-		for _, l7policy := range listener.L7Policies {
-			if l7policy.ID == id {
-				if l7policy.ProvisioningStatus == "" {
-					log.Printf("[DEBUG] Got an empty provisioning status response for the %s L7 Policy, falling back to ACTIVE", id)
-					return l7policy, "ACTIVE", nil
-				}
-				log.Printf("[DEBUG] Found %s provisioning status for the %s L7 Policy", l7policy.ProvisioningStatus, id)
-				return l7policy, l7policy.ProvisioningStatus, nil
-			}
-
-			for _, l7rule := range l7policy.L7rules {
-				if l7rule.ID == id {
-					if l7rule.ProvisioningStatus == "" {
-						log.Printf("[DEBUG] Got an empty provisioning status response for the %s L7 Rule, falling back to ACTIVE", id)
-						return l7rule, "ACTIVE", nil
+		switch resourceType {
+		case "listener":
+			for _, listener := range statuses.Loadbalancer.Listeners {
+				if listener.ID == resourceID {
+					if listener.ProvisioningStatus != "" {
+						return listener, listener.ProvisioningStatus, nil
 					}
-					log.Printf("[DEBUG] Found %s provisioning status for the %s L7 Rule", l7rule.ProvisioningStatus, id)
-					return l7rule, l7rule.ProvisioningStatus, nil
 				}
 			}
-		}
-		*/
-	}
+			listener, err := listeners.Get(lbClient, resourceID).Extract()
+			return listener, "ACTIVE", err
 
-	/* Waiting for https://github.com/gophercloud/gophercloud/issues/1366 */
-	for _, pool := range statuses.Loadbalancer.Pools {
-		if pool.ID == id {
-			if pool.ProvisioningStatus == "" {
-				log.Printf("[DEBUG] Got an empty provisioning status response for the %s Pool, falling back to ACTIVE", id)
-				return pool, "ACTIVE", nil
-			}
-			log.Printf("[DEBUG] Found %s provisioning status for the %s Pool", pool.ProvisioningStatus, id)
-			return pool, pool.ProvisioningStatus, nil
-		}
-
-		if pool.Monitor.ID == id {
-			if pool.Monitor.ProvisioningStatus == "" {
-				log.Printf("[DEBUG] Got an empty provisioning status response for the %s Monitor, falling back to ACTIVE", id)
-				return pool.Monitor, "ACTIVE", nil
-			}
-			log.Printf("[DEBUG] Found %s provisioning status for the %s Monitor", pool.Monitor.ProvisioningStatus, id)
-			return pool.Monitor, pool.Monitor.ProvisioningStatus, nil
-		}
-
-		for _, member := range pool.Members {
-			if member.ID == id {
-				if member.ProvisioningStatus == "" {
-					log.Printf("[DEBUG] Got an empty provisioning status response for the %s Member, falling back to ACTIVE", id)
-					return member, "ACTIVE", nil
+		case "pool":
+			for _, pool := range statuses.Loadbalancer.Pools {
+				if pool.ID == resourceID {
+					if pool.ProvisioningStatus != "" {
+						return pool, pool.ProvisioningStatus, nil
+					}
 				}
-				log.Printf("[DEBUG] Found %s provisioning status for the %s member", member.ProvisioningStatus, id)
-				return member, member.ProvisioningStatus, nil
 			}
+			pool, err := pools.Get(lbClient, resourceID).Extract()
+			return pool, "ACTIVE", err
+
+		case "monitor":
+			for _, pool := range statuses.Loadbalancer.Pools {
+				if pool.Monitor.ID == resourceID {
+					if pool.Monitor.ProvisioningStatus != "" {
+						return pool.Monitor, pool.Monitor.ProvisioningStatus, nil
+					}
+				}
+			}
+			monitor, err := monitors.Get(lbClient, resourceID).Extract()
+			return monitor, "ACTIVE", err
+
+		case "member":
+			for _, pool := range statuses.Loadbalancer.Pools {
+				for _, member := range pool.Members {
+					if member.ID == resourceID {
+						if member.ProvisioningStatus != "" {
+							return member, member.ProvisioningStatus, nil
+						}
+					}
+				}
+			}
+			return "", "DELETED", nil
 		}
-	}
 
-	err404 := gophercloud.ErrDefault404{
-		gophercloud.ErrUnexpectedResponseCode{
-			BaseError: gophercloud.BaseError{
-				DefaultErrString: fmt.Sprintf("Unable to to find the %s object from the Load Balancer %s statuses tree", id, lbID)},
-		},
+		return nil, "", fmt.Errorf("An unexpected error occurred querying the status of %s %s by loadbalancer %s", resourceType, resourceID, lbID)
 	}
-
-	return nil, "", err404
 }
